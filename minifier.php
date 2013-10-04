c1d20c49090d7a8760c42dcac7a680df2e9302bf
w3d2nl <j.copier@w3d2.nl>
2013-10-04 17:31:38 +0200

    Update minifier.php
    Make it is not possible to insert minified-group-urls using:
    ```
    <script type="text/javascript" src="<?=Minifier::make( 'js' )?>"></script>
    ```

--- minifier.php
+++ minifier.php
@@ -13,20 +13,21 @@
 class Minifier
 {
     /**
-     * Create a new image element from an email address.
-     * @param  array  $files array of the files to minify and combine
-     * @return string The source for combined url
+     * @param  array   $files array of the files to minify and combine
+     * @param  boolean Optional parameter to get a group url
+     * @return string  The source for combined url
      */
-    public static function make($files)
+    public static function make( $files, $group=false )
     {
         require_once 'libraries/min/utils.php';
 
-        // cast to array
-        if(!is_array($files))
-            $files = array($files);
+        if ( (! is_bool( $group ) ) || $group === false ) {
+            // cast to array
+            if(! is_array( $files ) ) $files = array( $files );
+        }
 
-        $uri = Minify_getUri($files);
-        $uri = str_replace('/min/', '/min/?', $uri);
+        $uri = Minify_getUri( $files );
+        $uri = str_replace( '/min/', '/min/?', $uri );
 
         return $uri;
     }
